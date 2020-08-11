7cf6c0b2ad88edfd2335654ce59ce92b88d08fc2
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-11 23:38:14 +0300

    Collector of all images in a carousel

---
+++ src/Components/TrainBox/CarouselBox/Data/CarouselData.js
@@ -0,0 +1,50 @@
+import Arena from '../../../../assets/images/TrainBox/Carousel/Arena.jpg';
+import Bridge from '../../../../assets/images/TrainBox/Carousel/Bridge.jpg';
+import Center from '../../../../assets/images/TrainBox/Carousel/Center.jpg';
+import Fountain from '../../../../assets/images/TrainBox/Carousel/Fountain.jpg';
+import Homes from '../../../../assets/images/TrainBox/Carousel/Homes.jpg';
+import Kunstkamera from '../../../../assets/images/TrainBox/Carousel/Kunstkamera.jpg';
+import Neva from '../../../../assets/images/TrainBox/Carousel/Neva.jpg';
+import Palace from '../../../../assets/images/TrainBox/Carousel/Palace.jpg';
+import Road from '../../../../assets/images/TrainBox/Carousel/Road.jpg';
+
+const CAROUSEL_DATA = [
+    {
+        src: Arena,
+        alt: 'Arena'
+    },
+    {
+        src: Bridge,
+        alt: 'Bridge'
+    },
+    {
+        src: Center,
+        alt: 'Center'
+    },
+    {
+        src: Fountain,
+        alt: 'Fountain'
+    },
+    {
+        src: Homes,
+        alt: 'Homes'
+    },
+    {
+        src: Kunstkamera,
+        alt: 'Kunstkamera'
+    },
+    {
+        src: Neva,
+        alt: 'Neva'
+    },
+    {
+        src: Palace,
+        alt: 'Palace'
+    },
+    {
+        src: Road,
+        alt: 'Road'
+    }
+]
+
+export default CAROUSEL_DATA;
