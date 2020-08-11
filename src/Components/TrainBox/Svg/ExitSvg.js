e56c913390daf385f6a376769a9d2f395b6b4367
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-11 23:27:09 +0300

    SVG files

---
+++ src/Components/TrainBox/Svg/ExitSvg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ExitSvg = () => {
+    return (
+        <>
+        <svg fill="none" viewBox="0 0 22 22">
+            <path d="M8 20H4C3.46957 20 2.96086 19.7893 2.58579 19.4142C2.21071 19.0391 2 18.5304 2 18V4C2 3.46957 2.21071 2.96086 2.58579 2.58579C2.96086 2.21071 3.46957 2 4 2H8" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M15 16L20 11L15 6" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M20 11H8" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>    
+        </>
+    )
+}
