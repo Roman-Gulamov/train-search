50e1bee62f21a93d4cb932c02f994340027ffc77
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-12 00:03:50 +0300

    Updates

--- src/Components/Router/getCookie.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-useless-escape */
-export const getCookie = (name) => {
-    const matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./assets/fonts/SF-Pro-Text/stylesheet.css";
-import "./assets/fonts/Roboto/stylesheet.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
-import { Direction } from './Components/Router/Direction';
-
-ReactDOM.render(<Direction />,document.getElementById('root'));
