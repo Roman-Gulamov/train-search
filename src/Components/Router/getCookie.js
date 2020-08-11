163017e2ff2f55575a6c4a76cbd20e2f00b1578d
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-11 23:26:19 +0300

    Coockie checker

---
+++ src/Components/Router/getCookie.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-useless-escape */
+export const getCookie = (name) => {
+    const matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
