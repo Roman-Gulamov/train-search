ea116bb9e1071a3a4aa9abd94b935aa35c93a573
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-11 23:39:53 +0300

    Ticket search page

---
+++ src/Components/TrainBox/TrainBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './trainBox.scss';
+import { exitClick } from './exitClick';
+import { ExitSvg } from './Svg/ExitSvg';
+import { Departures } from './Departures/Departures';
+import { CarouselBox } from './CarouselBox/CarouselBox';
+import { DirectTrains } from './DirectTrains/DirectTrains';
+
+
+export const TrainBox = () => {
+    return (
+        <div className='trainBox'>
+            <div className='login'>
+                <a href='/authorization' onClick={exitClick}>
+                    <span>Выйти</span>
+                    <ExitSvg />
+                </a>
+            </div>
+            <div className='trainBox__wrapper'>
+                <Departures />
+                <CarouselBox />
+                <DirectTrains />
+            </div>
+        </div>
+    )
+}
