d202d0324f48b347098d3e7b0a1df90020f351a1
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-11 23:42:28 +0300

    Ticket page

---
+++ src/Components/TrainBox/DirectTrains/DirectTrains.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import './directTrains.scss';
+import { Context } from '../../context';
+import { DirectMap } from './DirectMap';
+
+
+export const DirectTrains = () => {
+    const initialState = () => Number(localStorage.getItem("isFavorite") || null);
+    const [isFavorite, setIsFavorite] = useState(initialState);
+
+    const onFavoriteHandler = (event) => {
+        const heart = event.target;
+        const heartId = event.target.getAttribute("data-heartid");
+
+        if (heart.classList.contains('fill')) {
+            setIsFavorite(isFavorite - 1);
+            localStorage.removeItem(`heart ${heartId}`);
+        } else {
+            setIsFavorite(isFavorite + 1);
+            localStorage.setItem(`heart ${heartId}`, 'fill');
+        }
+    }
+
+    useEffect(() => localStorage.setItem("isFavorite", isFavorite), [isFavorite]);
+
+    return (
+        <Context.Provider value={(event) => onFavoriteHandler(event)}>
+            <>
+                <div className='countFavor'>
+                    <span>Добавлено в Избранное: <span className='text-primary'>{isFavorite}</span> рейсов</span>
+                </div>
+                <div className='direct mt-4'>
+                    <DirectMap />
+                </div>
+            </>
+        </Context.Provider>
+    )
+}
