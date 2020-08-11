16fa03319d595990bc70a7f4722ff9d41e53e9df
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-11 23:37:40 +0300

    Configuring React-Slick

---
+++ src/Components/TrainBox/CarouselBox/slickSettings.js
@@ -0,0 +1,12 @@
+export const slickSettings = {
+    dots: false,
+    adaptiveHeight: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true
+};
