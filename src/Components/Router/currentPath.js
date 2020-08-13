1e17022f9730a08e75b7e4f8668dd9c3d384c221
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-13 06:20:52 +0300

    Added token check to condition

--- src/Components/Router/currentPath.js
+++ src/Components/Router/currentPath.js
@@ -4,7 +4,7 @@ import { TrainBox } from '../TrainBox/TrainBox';
 import { Redirect } from 'react-router-dom';
 
 export const currentPath = () => {
-    if (getCookie('login') && getCookie('password')) {
+    if (getCookie('username') && getCookie('password') && localStorage.getItem("token")) {
         return <TrainBox />
     } else {
         return <Redirect to="/authorization" />
