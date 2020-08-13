f8a0b1e02ad8caea8458f0d0b0880710e3fa8a93
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-13 06:21:21 +0300

    The component is split into 2 more blocks

--- src/Components/TrainBox/TrainBox.jsx
+++ src/Components/TrainBox/TrainBox.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './trainBox.scss';
-import { exitClick } from './exitClick';
-import { ExitSvg } from './Svg/ExitSvg';
+import { Users } from './Users/Users'
 import { Departures } from './Departures/Departures';
 import { CarouselBox } from './CarouselBox/CarouselBox';
 import { DirectTrains } from './DirectTrains/DirectTrains';
@@ -10,12 +9,7 @@ import { DirectTrains } from './DirectTrains/DirectTrains';
 export const TrainBox = () => {
     return (
         <div className='trainBox'>
-            <div className='login'>
-                <a href='/train-search/#/authorization' onClick={exitClick}>
-                    <span>Выйти</span>
-                    <ExitSvg />
-                </a>
-            </div>
+            <Users />
             <div className='trainBox__wrapper'>
                 <Departures />
                 <CarouselBox />
