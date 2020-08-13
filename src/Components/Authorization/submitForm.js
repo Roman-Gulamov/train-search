52450653d10c26daa8e4513dc97ee65312ef0248
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-13 06:20:12 +0300

    Added axios request storing the received token in localstorage

--- src/Components/Authorization/submitForm.js
+++ src/Components/Authorization/submitForm.js
@@ -1,12 +1,24 @@
 import { createBrowserHistory } from "history";
+import Axios from "axios";
 
 export const submitForm = (values) => {
-    const history = createBrowserHistory();
-    const date = new Date(Date.now() + 86400e3);
-    document.cookie = `login = ${values.login}; expires= ${date}`; 
-    document.cookie = `password = ${values.password}; expires= ${date}`;
-    
-    history.push('/train-search/#/search');
-    history.go(0);
-    window.location.reload(); // for mobile phone
+    const axios = Axios.create();
+    axios.post("http://emphasoft-test-assignment.herokuapp.com/api-token-auth/", {...values}, {
+        header: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    })
+    .then((response) => {
+        localStorage.setItem("token", response.data.token);
+    }).then(() => {
+        const history = createBrowserHistory();
+        const date = new Date(Date.now() + 86400e3);
+
+        document.cookie = `username = ${values.username}; expires= ${date}`; 
+        document.cookie = `password = ${values.password}; expires= ${date}`;
+
+        history.push('/train-search/#/search');
+        history.go(0);
+        window.location.reload(); // for mobile phone
+    })
 }
