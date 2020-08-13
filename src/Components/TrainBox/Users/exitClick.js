82abb446e6227b724bf1f2b66380190762bfe98c
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-13 06:24:26 +0300

    Cookie name changed

---
+++ src/Components/TrainBox/Users/exitClick.js
@@ -0,0 +1,16 @@
+import { createBrowserHistory } from "history";
+
+export const exitClick = (event) => {
+    event.preventDefault();
+
+    const history = createBrowserHistory();
+    const oldDate = 'Thu, 01 Jan 1970 00:00:00 GMT';
+    
+    document.cookie = `username = ; path=/train-search; expires=${oldDate}`;
+    document.cookie = `password = ; path=/train-search; expires= ${oldDate}`;
+    localStorage.clear();
+    
+    history.push('/train-search/#/authorization');
+    history.go(0);
+    window.location.reload(); // for mobile phone
+}
