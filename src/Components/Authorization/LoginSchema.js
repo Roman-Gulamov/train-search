51937674412abc0b5aa91f61791f89dc966efebb
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-23 00:06:05 +0300

    Updated schema

--- src/Components/Authorization/LoginSchema.js
+++ src/Components/Authorization/LoginSchema.js
@@ -2,12 +2,11 @@ import * as Yup from "yup";
 
 export const LoginSchema = Yup.object().shape({
     username: Yup.string()
-        .matches(/test_super/, "Неверный логин")
-        .max(10, "Слишком длинный логин")
+        .min(3, "В логине должно быть не менее 3 симоволов")
+        .max(30, "Слишком длинный логин")
         .required("Пожалуйста, введите логин"),
     password: Yup.string()
+        .matches(/^[^А-Яа-яЁё]+$/, "Не используйте кириллицу")
         .min(8, "В пароле должно быть не менее 8 симоволов")
-        .matches(/Nf<U4f<rDbtDxAPn/, "Неправильный пароль")
-        .max(16, "Слишком длинный пароль")
         .required("Пожалуйста, введите пароль")
 });
